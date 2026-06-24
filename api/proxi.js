2bb114bf3e1da4712f579f0680acf16f9ed2d9cf
232102582Dann <zulramadhan876@gmail.com>
2026-06-24 16:41:57 +0700

    Update proxi.js

--- api/proxi.js
+++ api/proxi.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   if (req.method === 'OPTIONS') return res.status(200).end();
 
   const apiKey = process.env.GEMINI_API_KEY;
-  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent`;
+  const url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}";
   try {
     const response = await fetch(url, {
       method: 'POST',
